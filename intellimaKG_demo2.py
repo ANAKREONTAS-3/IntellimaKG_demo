47343c4fef9368da902c16a8aaf90cbca6f31e2e
ANAKREONTAS-3 <konstantinos66100@gmail.com>
2025-11-02 20:48:48 +0200

    Add files via upload

--- intellimaKG_demo2.py
+++ intellimaKG_demo2.py
@@ -137,7 +137,7 @@ header_html = f"""
 st.markdown(header_html, unsafe_allow_html=True)
 
 st.markdown(
-    "<div style='text-aling:center; color:#444; background-color:#f2f2f2; "
+    "<div style='text-aling:center; color:#444; background-color:#f2f2f2;>"
     "padding:12px; border-radius:8px; font-size:16px;'>"
     "Χρησιμοποιήστε τον demo κωδικό που σας δόθηκε.<br>"
     "Καλή σας απόλαυση!"
